a6491f3c9de0b4e9bc28d4d688ab0674c0056fee
Andrey Smirnov <smira@qik.com>
2010-10-19 16:13:51 +0400

    One more version bump to cope with wrong package uploaded.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ ext_modules=[
 
 setup(
     name = "numa",
-    version = '1.2',
+    version = '1.3',
     description = "Interface to numa(3) Linux API for Python",
     author = 'Andrey Smirnov',
     author_email = 'me@smira.ru',
