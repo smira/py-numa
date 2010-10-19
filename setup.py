6eafa0256fecb643d680e23e179ddc52fab01407
Andrey Smirnov <smira@qik.com>
2010-10-19 16:09:40 +0400

    Version bump.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ ext_modules=[
 
 setup(
     name = "numa",
-    version = '1.1',
+    version = '1.2',
     description = "Interface to numa(3) Linux API for Python",
     author = 'Andrey Smirnov',
     author_email = 'me@smira.ru',
